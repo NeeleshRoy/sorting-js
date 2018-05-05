cb5c7b033888cf2ef6766c450ab837c1bc132dfc
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-05 15:11:39 +0530

    TESTBED: complete

--- src/utils/ArrayTestBed.js
+++ src/utils/ArrayTestBed.js
@@ -9,7 +9,7 @@ export class ArrayTestBed {
     for (let i = 0; i < this.numElements; i++) {
       let randomNumber = this.getRandomNumber();
       if (random) {
-        while(this.dataStore.indexOf(randomNumber) > -1) {
+        while (this.dataStore.indexOf(randomNumber) > -1) {
           randomNumber = this.getRandomNumber();
         }
         this.dataStore[i] = randomNumber;
@@ -26,5 +26,30 @@ export class ArrayTestBed {
 
   getData() {
     return this.dataStore;
+  }
+
+  insert(element) {
+    this.dataStore[this.pos++] = element;
+  }
+
+  clear() {
+    this.dataStore = [];
+    this.pos = 0;
+  }
+
+  toString() {
+    let returnStr = '';
+    this.dataStore.forEach((value) => {
+      returnStr += `${value},`;
+    });
+    return returnStr;
+  }
+
+  swap(arr, index1, index2) {
+    const arrTemp = arr;
+    const temp = arrTemp[index1];
+    arrTemp[index1] = arrTemp[index2];
+    arrTemp[index2] = temp;
+    return arrTemp;
   }
 }
--- test/ArrayTestBed.test.js
+++ test/ArrayTestBed.test.js
@@ -47,7 +47,7 @@ describe('ArrayTestBed', () => {
     it('Should set random data in datastore', () => {
       const test = new ArrayTestBed(10);
       test.setData(false);
-      console.log(test.dataStore);
+
       expect(test.dataStore.length).to.eql(10);
     });
 
@@ -66,8 +66,61 @@ describe('ArrayTestBed', () => {
       const test = new ArrayTestBed(10);
       test.setData();
       const out = test.getData();
-      
+
       expect(out.length).to.eql(10);
+    });
+
+  });
+
+  describe('ArrayTestBed - clear()', () => {
+
+    it('Should clear the datastore', () => {
+      const test = new ArrayTestBed(10);
+      test.setData();
+      expect(test.dataStore.length).to.eql(10);
+      test.clear();
+      expect(test.dataStore).to.eql([]);
+      expect(test.pos).to.eql(0);
+    });
+
+  });
+
+  describe('ArrayTestBed - toString()', () => {
+
+    it('Should return the elements of datastore in string format', () => {
+      const test = new ArrayTestBed();
+      test.dataStore = [1, 2, 3];
+      const out = test.toString();
+
+      expect(out).to.eql('1,2,3,');
+    });
+
+  });
+
+  describe('ArrayTestBed - swap()', () => {
+
+    it('Should swap the array elements', () => {
+      const test = [1, 2, 3, 4];
+      const arrClass = new ArrayTestBed();
+
+      const out = arrClass.swap(test, 1, 2);
+
+      expect(out).to.eql([1, 3, 2, 4]);
+    });
+
+  });
+
+  describe('ArrayTestBed - insert()', () => {
+
+    it('Should insert the elements inside the array', () => {
+      const test = new ArrayTestBed();
+
+      test.insert(1);
+      test.insert(2);
+      test.insert(3);
+      test.insert(4);
+
+      expect(test.dataStore).to.eql([1, 2, 3, 4]);
     });
 
   });
