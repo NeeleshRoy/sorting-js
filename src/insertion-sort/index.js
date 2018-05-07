17706aa25e4c70bba56819e645362f1d6c301954
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-07 15:25:22 +0530

    Insertion sort

--- src/insertion-sort/index.js
+++ src/insertion-sort/index.js
@@ -1,12 +1,13 @@
 export function insertionSort(arr) {
   const unsorted = arr;
-  let temp, inner;
-  
-  for (let outer = 1; outer <= unsorted.length-1; ++outer) {
+  let temp;
+  let inner;
+
+  for (let outer = 1; outer <= unsorted.length - 1; ++outer) {
     temp = unsorted[outer];
     inner = outer;
-    while (inner > 0 && (unsorted[inner-1] >= temp)) {
-      unsorted[inner] = unsorted[inner-1];
+    while (inner > 0 && (unsorted[inner - 1] >= temp)) {
+      unsorted[inner] = unsorted[inner - 1];
       --inner;
     }
     unsorted[inner] = temp;
