7d488f5a095dd06212db725d2b83afb760311f70
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-10 22:40:27 +0530

    refactored approach

--- src/insertion-sort/index.js
+++ src/insertion-sort/index.js
@@ -1,16 +1,14 @@
 export function insertionSort(arr) {
   const unsorted = arr;
-  let temp;
-  let inner;
 
-  for (let outer = 1; outer <= unsorted.length - 1; ++outer) {
-    temp = unsorted[outer];
-    inner = outer;
-    while (inner > 0 && (unsorted[inner - 1] >= temp)) {
-      unsorted[inner] = unsorted[inner - 1];
-      --inner;
+  for (let i = 1; i < unsorted.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (unsorted[i] < unsorted[j]) {
+        const spliced = unsorted.splice(i, 1);
+        unsorted.splice(j, 0, spliced[0]);
+      }
     }
-    unsorted[inner] = temp;
   }
+
   return unsorted;
 }
