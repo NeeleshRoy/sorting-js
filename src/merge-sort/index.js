5f55a850ab30feef2a4f47f6dc78b46e36bff7a4
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-12 10:48:13 +0530

    mergesort algorithm

---
+++ src/merge-sort/index.js
@@ -0,0 +1,34 @@
+function stitch(left, right) {
+  const results = [];
+  if (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  while (left.length) {
+    results.push(left.shift());
+  }
+
+  while (right.length) {
+    results.push(right.shift());
+  }
+
+  return results;
+}
+
+export const mergeSort = (arr) => {
+  const unsorted = arr;
+  if (unsorted.length < 2) {
+    return unsorted;
+  }
+
+  const length = unsorted.length;
+  const middle = Math.floor(length / 2);
+  const left = unsorted.slice(0, middle);
+  const right = unsorted.slice(middle, length);
+
+  return stitch(mergeSort(left), mergeSort(right));
+};
