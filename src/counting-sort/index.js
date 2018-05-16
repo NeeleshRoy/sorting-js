192c41420a8af8f175847d2e1c987f5401a1f45a
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-16 18:16:49 +0530

    counting-sort

--- src/counting-sort/index.js
+++ src/counting-sort/index.js
@@ -1,13 +1,17 @@
-export const countingSort = (arr) => {
-	var i,
-		z = 0,
-		count = [];
+export const countingSort = (unsorted, min, max) => {
+	const arr = unsorted;
+	let i = 0;
+	let z = 0;
+	const count = [];
+
 	for (i = min; i <= max; i++) {
 		count[i] = 0;
 	}
+
 	for (i = 0; i < arr.length; i++) {
 		count[arr[i]]++;
 	}
+
 	for (i = min; i <= max; i++) {
 		while (count[i]-- > 0) {
 			arr[z++] = i;
