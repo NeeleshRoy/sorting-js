bb4da99424de375a44c54f098a7087d94fa3ef9f
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-17 22:02:52 +0530

    added counting sort to release

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import insertionSort from './insertion-sort/index';
 import selectionSort from './selection-sort/index';
 import { mergeSort } from './merge-sort/index';
 import { quickSort } from './quick-sort/index';
+import { countingSort } from './counting-sort/index';
 
 module.exports = {
-  ArrayTestBed,
-  bubbleSort,
-  insertionSort,
-  selectionSort,
-  mergeSort,
-  quickSort,
+	ArrayTestBed,
+	bubbleSort,
+	insertionSort,
+	selectionSort,
+	mergeSort,
+	quickSort,
+	countingSort
 };
