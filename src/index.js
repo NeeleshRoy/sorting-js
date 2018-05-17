06467a2adfbe197b27cd25c98a7ece7d92306047
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-17 23:08:36 +0530

    fixed exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { ArrayTestBed } from './utils/ArrayTestBed';
-import bubbleSort from './bubble-sort/index';
-import insertionSort from './insertion-sort/index';
-import selectionSort from './selection-sort/index';
+import { bubbleSort } from './bubble-sort/index';
+import { insertionSort } from './insertion-sort/index';
+import { selectionSort } from './selection-sort/index';
 import { mergeSort } from './merge-sort/index';
 import { quickSort } from './quick-sort/index';
 import { countingSort } from './counting-sort/index';
